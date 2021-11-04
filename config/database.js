dd44594296c2729c9ae24963ddc114918c25d7d2
samekh resh <samekh.resh@gmail.com>
2021-11-04 10:00:57 -0400

    changes to database.js to push to heroku.

--- config/database.js
+++ config/database.js
@@ -7,6 +7,6 @@ let db_name = process.env.DB_PASSWORD
 module.exports = {
 
 
-    "url" : `mongodb+srv://samekh:${db_name}@21savage.xlrtw.mongodb.net/demo?retryWrites=true&w=majority`,
-    "dbName": "coffeeShop"
+    url : `mongodb+srv://samekh:${db_name}@21savage.xlrtw.mongodb.net/demo?retryWrites=true&w=majority`,
+    dbName: "coffeeShop"
 };
